bba43b345a1d19bd423235c2c035e1e25896e0a4
Jin Lim <jin.lim@flender.ie>
2025-08-06 16:43:44 +0100

    commit

---
+++ astro.config.mjs
@@ -0,0 +1,62 @@
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import rehypeExternalLinks from "rehype-external-links";
+import remarkParseContent from "./src/lib/utils/remarkParseContent.ts";
+import {
+  parseTomlToJson,
+  reloadOnTomlChange,
+} from "./src/lib/utils/tomlUtils.ts";
+import { enabledLanguages } from "./src/lib/utils/i18nUtils.ts";
+
+const config = parseTomlToJson();
+
+let {
+  seo: { sitemap: sitemapConfig },
+  settings: {
+    multilingual: { showDefaultLangInUrl, defaultLanguage },
+  },
+} = config;
+
+// https://astro.build/config
+export default defineConfig({
+  outDir: './docs',  // <== This is where build output goes
+  base: '/astro-orange/',  // <== This is CRITICAL for GitHub Pages!
+  site: 'https://limjinsun.github.io/astro-orange/',  // Full URL
+
+  site: config.site.baseUrl ? config.site.baseUrl : "http://examplesite.com",
+  trailingSlash: config.site.trailingSlash ? "always" : "never",
+  i18n: {
+    locales: enabledLanguages,
+    defaultLocale: defaultLanguage,
+    routing: {
+      redirectToDefaultLocale: showDefaultLangInUrl ? false : true,
+      prefixDefaultLocale: showDefaultLangInUrl,
+    },
+  },
+  integrations: [react(), sitemapConfig.enable ? sitemap() : null, mdx()],
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          rel: "noopener noreferrer nofollow",
+          target: "_blank",
+        },
+      ],
+    ],
+    remarkPlugins: [remarkParseContent],
+
+    // Code Highlighter https://github.com/shikijs/shiki
+    shikiConfig: {
+      theme: "light-plus", // https://shiki.style/themes
+      wrap: false,
+    },
+    extendDefaultPlugins: true,
+  },
+  vite: {
+    plugins: [tailwindcss(), reloadOnTomlChange()],
+  },
+});
